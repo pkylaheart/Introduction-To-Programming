eafc4c712d85b4e127aac202f8a7a86ffb85eb42
unknown <kheartpineda@gmail.com>
2024-12-17 07:07:11 +0800

    updated project

--- 1st_Semester_Project/Menu_Pineda.py
+++ 1st_Semester_Project/Menu_Pineda.py
@@ -1,4 +1,3 @@
-
 import os
 
 def clear_screen():
@@ -9,7 +8,7 @@ def clear_screen():
 
 def print_menu():
     while True:
-        print("Let's get started with python printing. Pick any topic of printing in python below. \n ╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╮\n\t     ⋘ PRINT STATEMENT ⋙ \n\n\t 1. PRINTING DATA TYPES \n\t 2. PRINTING WITH ESCAPE CHARACTER \n\t 3. COMMENTS \n\t 4. QUIT \n ╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╯")
+        print("Let's get started with python printing. Pick any topic of printing in python below. \n ╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╮\n\t     ⋘ PRINT STATEMENT ⋙ \n\n\t 1. PRINTING DATA TYPES \n\t 2. PRINTING WITH ESCAPE CHARACTER \n\t 3. COMMENTS \n\t 4. QUIT \n ╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╯")
         print_ask = input("Enter number:  ")  
 
         clear_screen()
@@ -50,28 +49,37 @@ def print_datatypes():
 
 def string():
     while True:
-            print("\t\t-- STRING --")
-            print("Code:") 
-            print("print('Hello World')\n")
-            print("Output:")
-            print("Hello World!\n")
-            back1 = input("--------------------- \nPress Any to go back.")
+        print("\t\t-- STRING --\n")
+        print("Explanation: A string is a piece of text, like words or sentences. It's anything inside quotes.")
+        print("Example Code:") 
+        print("print('Hello World')\n")
+        print("Output:")
+        print("Hello World!\n\n")
+        print("Try printing a String!")
+        string = input("Input a word or letter: ")
+        print(string)
+            
+        back1 = input("--------------------- \nPress Any to go back.")
 
-            clear_screen()
+        clear_screen()
 
-            if input == input:
-                print_datatypes()
-                break
-            else: 
-                print("invalid input")
-                continue
+        if input == input:
+            print_datatypes()
+            break
+        else: 
+            print("invalid input")
+            continue
+
 def boolean():
     while True:
             print("\t\t-- BOOLEAN --")
-            print("Code:") 
-            print("print(True)\n")
+            print("A boolean is a type that can only have one of two values: True (yes) or False (no).\n")
+            print("Example Code:") 
+            print("is_raining = True")
+            print("if is_raining:")
+            print("\tprint('Take an umbrella!')\n") 
             print("Output:")
-            print("True\n")
+            print("Take an umbrella!\n")
             back1 = input("--------------------- \nPress Any to go back.")
 
             clear_screen()
@@ -84,40 +92,55 @@ def boolean():
                 continue
 
 def integer():
+    print("\t\t-- INTEGER --")
+    print("An integer is a whole number without any decimal or fractional part.")
+    print("Example Code:") 
+    print("print(5)\n")
+    print("Output:")
+    print("5\n")    
+    print("Try it!")
+
     while True:
-            print("\t\t-- INTEGER --")
-            print("Code:") 
-            print("print(5)\n")
-            print("Output:")
-            print("5\n")
-            back1 = input("--------------------- \nPress Any to go back.")
-
-            clear_screen()
-
-            if input == input:
+        user_input = input("\nPlease enter a Number: ")
+        try:
+            number = int(user_input)
+            print(f"You entered the integer: {number}")
+            ask = input("--------------------- \nPress any key to go back: ")
+                
+            clear_screen()  
+                
+            if ask == ask:
                 print_datatypes()
                 break
-            else: 
-                print("invalid input")
-                continue
+        except ValueError:
+            print("That is not a valid number.")
+        
+           
 
 def float_():
+    print("\t\t-- FLOAT --")
+    print("\nA float is a number that can have a decimal point.")
+    print("Example Code:") 
+    print("print(2.0)\n")
+    print("Output:")
+    print("2.0\n")   
+    print("Try it!")   
+
     while True:
-            print("\t\t-- FLOAT --")
-            print("Code:") 
-            print("print(2.0)\n")
-            print("Output:")
-            print("2.0\n")
-            back1 = input("--------------------- \nPress Any to go back.")
-
-            clear_screen()
-
-            if input == input:
+        user_input = input("Please enter a float: ")
+        if '.' not in user_input:
+            print("That is not a valid float (needs to have a decimal point). Please enter a valid float.")
+            continue
+        else:
+            print(f"You entered the float: {user_input}")
+            ask = input("--------------------- \nPress any key to go back: ")
+            
+            clear_screen()  
+            
+            if ask == ask:
                 print_datatypes()
                 break
-            else: 
-                print("invalid input")
-                continue
+    
 
 def escape_char():
     print("\t     -- PRINTING WITH ESCAPE CHARACTERS --")                 
@@ -229,7 +252,99 @@ def comments():
     back1 = input("--------------------- \nPress Any to go back.")
     
     clear_screen()
-    
+
+def arithmetic_operations():
+    while True:
+        print("\t\t-- ARITHMETIC OPERATIONS --")
+        print("Choose an operation:")
+        print("1. Addition (+) \t\t2. Subtraction (-)")
+        print("3. Multiplication (*) \t\t4. Division (/) \t\t5. Back\n")
+        
+        choice = input("Enter Number: ")
+        
+        clear_screen()
+
+        if choice == '1': 
+            addition()        
+        elif choice == '2': 
+            substraction()
+        elif choice == '3':  
+            multiplication()
+        elif choice == '4':  
+            division()
+        elif choice == '5':
+            menu()
+            break
+        else: 
+            print("invalid input")
+            clear_screen()
+            arithmetic_operations()
+def addition():
+    print("\t\t\tADDITION OPERATION\n")
+    print("Explanation: Addition adds two numbers together.\n")
+    print("Code:")
+    print("num1 = 5")
+    print("num2 = 3")
+    print("result = num1 + num2\n")
+    result = 5 + 3
+    print(f"Output: {result}")
+    input("--------------------- \nPress Any to go back.")
+
+    clear_screen()
+
+    if input == input:
+        arithmetic_operations()
+        
+            
+def substraction():
+    print("\t\tSUBSTRACTION OPERATION\n")
+    print("Explanation: Subtraction subtracts the second number from the first.\n")
+    print("Code:")
+    print("num1 = 5")
+    print("num2 = 3")
+    print("result = num1 - num2\n")
+    result = 5 - 3
+    print(f"Output: {result}")
+    input("--------------------- \nPress Any to go back.")
+
+    clear_screen()
+
+    if input == input:
+        arithmetic_operations()
+        
+def multiplication():
+    print("\t\tMULTIPLICATION OPERATION\n")
+    print("Explanation: Multiplication multiplies two numbers together.\n")
+    print("Code:")
+    print("num1 = 5")
+    print("num2 = 3")
+    print("result = num1 * num2\n")
+    result = 5 * 3
+    print(f"Output: {result}")
+    input("--------------------- \nPress Any to go back.")
+
+    clear_screen()
+
+    if input == input:
+        arithmetic_operations()
+        
+def division():
+    print("\t\tDIVISION OPERATION\n")
+    print("Explanation: Subtraction subtracts the second number from the first.\n")
+    print("Code:")
+    print("num1 = 6")
+    print("num2 = 3")
+    print("result = num1 / num2\n")
+    result = 6 / 3
+    print(f"Output: {result}")
+    input("--------------------- \nPress Any to go back.")
+
+    clear_screen()
+
+    if input == input:
+        arithmetic_operations()
+                
+                        
 def py_variables_menu():
     while True:
         print("Let's get started with Python Variables. Pick any topic of creating variables in python below. \n ╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╮\n\t     ⋘ VARIABLES ⋙ \n\n\t 1. ASSIGN VARIABLES \n\t 2. OUTPUT VARIBLES \n\t 3. CASTING \n\t 4. QUIT \n ╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╯")
@@ -247,25 +362,38 @@ def py_variables_menu():
             menu() 
             break
         else:
-            print("Invalid Input.. Try again \n")
+            print("Invalid Input.. Try again \n"
+            )
 def create_var():
-     while True:
+    while True:
         print("\t\t-- CREATING VARIABLES --")
         print("Variables are for storing data called Values.")
-        print("\nCode:") 
+        print("\nExample Code:") 
         print("x = 21\ny = 'Kyla'\n\nprint(x)\nprint(y)\n")
         print("Output:")
         print("5\nKyla")
-        back1 = input("--------------------- \nPress Any to go back.")
+        print("\nTry it!")
+        while True:
+            var = input("Enter variable name: ")
+            if var == " " or var == '':
+                print("No input found, try again")
+                continue
+            elif var[0].isdigit():
+                print("Invalid variable name: Variable names cannot start with a number. Try again.")   
+                continue 
+            elif var.isidentifier():       
+                val = input("Enter value: ")
+                print(f"{var} = {val}")
 
-        clear_screen()
+                back1 = input("--------------------- \nPress Any to go back.")
 
-        if input == input:
-           py_variables_menu()
-           break
-        else: 
-            print("invalid input")
-            continue
+                clear_screen()
+                if input == input:
+                    py_variables_menu()
+                    break
+                else: 
+                    print("invalid input")
+                    continue
 
 def multi_val():
     while True:
@@ -307,12 +435,13 @@ def py_conditional_menu():
     while True:
         print("Let's get started with Python Conditional Statements.")
         print("╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╮")
-        print("\t     ⋘ CONDITIONAL STATEMENTS ⋙")
-        print("\t 1. IF STATEMENT")
-        print("\t 2. ELIF STATEMENT")
-        print("\t 3. ELSE STATEMENT")
-        print("\t 4. LOGICAL OPERATORS")
-        print("\t 5. QUIT")
+        print("\t⋘ CONDITIONAL STATEMENTS ⋙")
+        print("\t  1. IF STATEMENT")
+        print("\t  2. ELIF STATEMENT")
+        print("\t  3. ELSE STATEMENT")
+        print("\t  4. LOGICAL OPERATORS")
+        print("\t  5. COMPARING OPERATORS")
+        print("\t  6. QUIT")
         print("╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╯")
         print_ask = input("Enter number: ")
 
@@ -327,11 +456,80 @@ def py_conditional_menu():
         elif print_ask == '4':
             logical_operators_menu()
         elif print_ask == '5':
+            comparing_operators()
+        elif print_ask == '6':
             menu()
             break
         else:
             print("Invalid Input.. Try again\n")
 
+def comparing_operators():
+    while True:
+        print("\t\t-- COMPARING OPERATORS --\n")
+        print("1. == (EQUAL)\t\t\t\t\t 2. != (NOT EQUAL)")
+        print("3. > (GREATER THAN)\t\t\t\t 4. < (LESS THAN)")
+        print("5. >= (GREATER THAN OR EQUAL)\t\t\t 6. <= (LESS THAN OR EQUAL)\n")
+        
+        operator = input(" Select an Operator (1-6) \n(Press 'Enter' to go back.): ")
+        clear_screen()
+
+        if operator == '1':
+            is_equal()
+        elif operator == '2':
+            is_notequal()
+        elif operator == '3':
+            greater()
+        elif operator == '4':
+            less()
+        elif operator == '5':
+            less_eq()
+        elif operator == '6':
+            greater_eq()
+        else: 
+            print("invalid input")
+            clear_screen()
+            py_conditional_menu()
+    
+    input("\nPress Enter to go back.")
+    clear_screen()
+    py_conditional_menu()
+
+def is_equal():
+    print("Checks if two values are exactly the same.\n")
+    print("Code: 5 == 5 \nOutput:", 5 == 5)
+    input("\nPress Enter to go back.")
+    clear_screen()
+    comparing_operators()
+def is_notequal():
+    print("Checks if two values are not the same.\n")
+    print("Code: 5 != 3 \nOutput:", 5 != 3)
+    input("\nPress Enter to go back.")
+    clear_screen()
+    comparing_operators()
+def greater():
+    print("Checks if the left value is larger than the right value.\n")
+    print("Code: 5 > 3 \nOutput:", 5 > 3)
+    input("\nPress Enter to go back.")
+    clear_screen()
+    comparing_operators()
+def less():
+    print("Checks if the left value is smaller than the right value.\n")
+    print("Code: 5 < 3 \nOutput:", 5 < 3)
+    input("\nPress Enter to go back.")
+    clear_screen()
+    comparing_operators()
+def greater_eq():
+    print("Checks if the left value is larger than or equal to the right value.\n")
+    print("Code: 5 >= 3 \nOutput:", 5 >= 3)
+    input("\nPress Enter to go back.")
+    clear_screen()
+    comparing_operators()
+def less_eq():
+    print("Checks if the left value is smaller than or equal to the right value.\n")
+    print("Code: 5 <= 3 \nOutput:", 5 <= 3)
+    input("\nPress Enter to go back.")
+    clear_screen()
+    comparing_operators()
 
 def if_statement():
     print("\t\t-- IF STATEMENT --")
@@ -377,21 +575,21 @@ def else_statement():
 
 def logical_operators_menu():
     while True:
-        print("Logical Operators Menu: \n")
-        print("1. and \n2. or \n3. not \n4. is \n5. is not \n6. Back")
+        print("\t\t-- LOGICAL OPERATORS -- \n")
+        print("1. AND \t\t\t4. OR \n2. NOT \t\t\t5. IS \n3. IS NOT \t\t6. BACK")
         operator_choice = input("\nEnter number:  ")
 
         clear_screen()
 
         if operator_choice == '1':
             and_operator()
-        elif operator_choice == '2':
-            or_operator()
-        elif operator_choice == '3':
-            not_operator()
         elif operator_choice == '4':
-            is_operator()
+            or_operator()
+        elif operator_choice == '2':
+            not_operator()
         elif operator_choice == '5':
+            is_operator()
+        elif operator_choice == '3':
             is_not_operator()
         elif operator_choice == '6':
             py_conditional_menu() 
@@ -474,7 +672,7 @@ def is_not_operator():
     print("Example:")
     print("x = [1, 2, 3]")
     print("y = [1, 2, 3]")
-    print("if x is not y: print('x and y are different objects')")
+    print("if x is not y\n\tprint('x and y are different objects')\nelse:\n\tprint('x and y are the same object)")
     print("\nOutput:")
     
     x = [1, 2, 3]
@@ -631,7 +829,8 @@ def py_list_menu():
         print("\t 1. LIST CREATION")
         print("\t 2. ACCESSING LIST ELEMENTS")
         print("\t 3. ADDING ELEMENTS")
-        print("\t 4. QUIT")
+        print("\t 4. REMOVE ITEM FROM LIST")
+        print("\t 5. QUIT")
         print("╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╯")
         print_ask = input("Enter number: ")
 
@@ -644,16 +843,31 @@ def py_list_menu():
         elif print_ask == '3':
             adding_elements()
         elif print_ask == '4':
+            remove_from_list()
+        elif print_ask == '5':
             menu()
             break
         else:
             print("Invalid Input.. Try again\n")
 
+def remove_from_list():
+    while True:
+        my_list = [1, 2, 3, 4, 5]
+        print("\t\t-- REMOVE ITEM FROM LIST --\n")
+        print(f"Original List: {my_list}")
+        print("my_list.remove(1)\n")
+        print("Output:")
+        my_list.remove(1)
+        print(my_list)
+        input("\nPress 'Enter' to go back.")
+        clear_screen()
+        py_list_menu()
+
 
 def list_creation():
     print("\t\t-- LIST CREATION --")
     print("Code:")
-    print("my_list = [1, 2, 3, 'hello']")
+    print("my_list = [1, 2, 3, 'hello']\n")
     print("Output:")
     my_list = [1, 2, 3, 'hello']
     print(my_list)
@@ -665,7 +879,7 @@ def list_creation():
 def accessing_list_elements():
     print("\t\t-- ACCESSING LIST ELEMENTS --")
     print("Code:")
-    print("my_list = [1, 2, 3, 'hello']\nprint(my_list[0]") 
+    print("my_list = [1, 2, 3, 'hello']\nprint(my_list[0]\n") 
     print("Output:")
     my_list = [1, 2, 3, 'hello']
     print(my_list[0])
@@ -677,7 +891,7 @@ def accessing_list_elements():
 def adding_elements():
     print("\t\t-- ADDING ELEMENTS TO A LIST --")
     print("Code:")
-    print("my_list = [1, 2, 3]\nmy_list.append(4)\nprint(my_list)")
+    print("my_list = [1, 2, 3]\nmy_list.append(4)\nprint(my_list)\n")
     print("Output:")
     my_list = [1, 2, 3]
     my_list.append(4)
@@ -688,7 +902,7 @@ def adding_elements():
 
 def menu():
     while True:
-        print("╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╮\n\t     ⋘ MAIN MENU ⋙ \n\n\t 1. PRINT STATEMENT \n\t 2. VARIABLES  \n\t 3. CONDITIONAL STATEMENTS \n\t 4. LOOP STATEMENTS \n\t 5. FUNCTIONS \n\t 6. LIST \n\t 7. QUIT \n\n ╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╯")
+        print("╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╮\n\t     ⋘ MAIN MENU ⋙ \n\n\t 1. PRINT STATEMENT \n\t 2. VARIABLES  \n\t 3. ARITHMETIC OPERATORS \n\t 4. CONDITIONAL STATEMENTS  \n\t 5. LOOP STATEMENTS \n\t 6. FUNCTIONS \n\t 7. LIST \n\t 8. QUIT \n\n ╰┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╯")
         option = input("Enter a number:  ")
         
         clear_screen()
@@ -700,36 +914,38 @@ def menu():
             py_variables_menu()
             break
         elif option == '3':
-            py_conditional_menu()
+            arithmetic_operations()
             break
         elif option == '4':
-            py_loop_menu()
+            py_conditional_menu()
             break
         elif option == '5':
-            py_functions_menu()
+            py_loop_menu()
             break
         elif option == '6':
-            py_list_menu()
+            py_functions_menu()
             break
         elif option == '7':
+            py_list_menu()
+            break
+        elif option == '8':
             print("Program Terminated..")
             break
         else:
             print("Invalid option, Try again!")
             continue
-
-
-while True:
-    name = input("Good day! May I know your name? ").title()
-    print(f"Hi, {name}")
-    start = input("Do you want to start the Program? (Yes/No) \n --> ")
+menu()
+# while True:
+#     name = input("Good day! May I know your name? ").title()
+#     print(f"Hi, {name}")
+#     start = input("Do you want to start the Program? (Yes/No) \n --> ")
     
-    clear_screen()
+#     clear_screen()
 
-    if start.lower() == "yes":
-        print("Lets Start!")
-        menu()
-        break
-    else:
-        print("Okay, Have a great day!")
-        break
+#     if start.lower() == "yes":
+#         print("Lets Start!")
+#         menu()
+#         break
+#     else:
+#         print("Okay, Have a great day!")
+#         break
